d9a5e338cc654564b3ac96eb7e890e4c4e84d1c4
vkavsek <vkavsek@gmail.com>
2023-12-03 00:02:02 +0100

    a bit of restructuring

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,33 +1,19 @@
 advent_of_code::solution!(2);
 
 struct Game {
-    r: usize,
-    g: usize,
-    b: usize,
+    colors: (usize, usize, usize),
 }
 
 impl Game {
     fn new(r: usize, g: usize, b: usize) -> Game {
-        Game { r, g, b }
+        Game { colors: (r, g, b) }
     }
 
     fn update_color(&mut self, color: &str, number: usize) {
         match color {
-            "red" => {
-                if number > self.r {
-                    self.r = number;
-                }
-            }
-            "green" => {
-                if number > self.g {
-                    self.g = number;
-                }
-            }
-            "blue" => {
-                if number > self.b {
-                    self.b = number;
-                }
-            }
+            "red" => self.colors.0 = self.colors.0.max(number),
+            "green" => self.colors.1 = self.colors.1.max(number),
+            "blue" => self.colors.2 = self.colors.2.max(number),
             _ => {}
         }
     }
@@ -41,9 +27,8 @@ fn get_games(input: &str) -> Vec<Game> {
             let (_game, data) = line.split_once(':').expect("error parsing input");
             for data_chunk in data.split(';') {
                 for color_data in data_chunk.split(',') {
-                    let color_data = color_data.trim();
-                    let (num, color) = color_data.split_once(' ').unwrap();
-                    let num = num.parse::<usize>().expect("Couldn't pass a number");
+                    let (num, color) = color_data.trim().split_once(' ').unwrap();
+                    let num = num.parse::<usize>().expect("couldn't parse a number");
 
                     game.update_color(color, num);
                 }
@@ -59,7 +44,7 @@ pub fn part_one(input: &str) -> Option<usize> {
     let res: usize = games
         .into_iter()
         .enumerate()
-        .filter(|(_, game)| game.r <= 12 && game.g <= 13 && game.b <= 14)
+        .filter(|(_, game)| game.colors.0 <= 12 && game.colors.1 <= 13 && game.colors.2 <= 14)
         .map(|(i, _)| i + 1)
         .sum();
 
@@ -69,7 +54,10 @@ pub fn part_one(input: &str) -> Option<usize> {
 pub fn part_two(input: &str) -> Option<usize> {
     let games = get_games(input);
 
-    let res = games.into_iter().map(|game| game.r * game.g * game.b).sum();
+    let res = games
+        .into_iter()
+        .map(|game| game.colors.0 * game.colors.1 * game.colors.2)
+        .sum();
 
     Some(res)
 }
