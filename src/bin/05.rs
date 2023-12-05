c5d70e423e6f2159fa745e1654cd875bf71b51dd
vkavsek <vkavsek@gmail.com>
2023-12-05 20:49:33 +0100

    multithreaded seed processing with rayon

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,100 +1,17 @@
 use std::collections::BTreeSet;
 
 use itertools::Itertools;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 advent_of_code::solution!(5);
-//
-// #[derive(Debug, Clone)]
-// struct DataMap {
-//     mappings: BTreeMap<u32, u32>,
-// }
-// impl DataMap {
-//     fn fill_datamap(&mut self, fill_to: usize) {
-//         for i in 0..=fill_to {
-//             self.mappings.entry(i as u32).or_insert(i as u32);
-//         }
-//     }
-// }
-//
-// #[derive(Debug, Clone)]
-// struct Almanach {
-//     data_maps: Vec<DataMap>,
-// }
-// impl Almanach {
-//     // Initialize an Almanach with empty DataMap inside.
-//     fn init() -> Almanach {
-//         Almanach {
-//             data_maps: vec![DataMap {
-//                 mappings: BTreeMap::new(),
-//             }],
-//         }
-//     }
-//     fn push_new_dm(&mut self) {
-//         self.data_maps.push(DataMap {
-//             mappings: BTreeMap::new(),
-//         });
-//     }
-//
-//     /// Inserts into the latest created DataMap
-//     /// If data_maps Vec is empty, this function returns None.
-//     /// Insertion is infallible, if the key already exists the value
-//     /// is updated and Some(key) is returned.
-//     fn insert_into_dm(&mut self, key_src: u32, value_dest: u32) -> Option<u32> {
-//         self.data_maps
-//             .last_mut()?
-//             .mappings
-//             .entry(key_src)
-//             .or_insert(value_dest);
-//         Some(key_src)
-//     }
-//
-//     /// Returns None if it can't find a KEY_SRC entry in any of the DataMaps.
-//     fn find_final_elem(&mut self, n: u32) -> Option<u32> {
-//         let mut key_src = n;
-//         let mut data_iter = self.data_maps.iter_mut().peekable();
-//         while let Some(data_map) = data_iter.next() {
-//             let dest = data_map
-//                 .mappings
-//                 .get(&key_src)
-//                 .unwrap_or(&key_src)
-//                 .to_owned();
-//             if let Some(next_data_map) = data_iter.peek_mut() {
-//                 next_data_map.fill_datamap(dest as usize);
-//             }
-//
-//             key_src = dest;
-//         }
-//         Some(key_src)
-//     }
-// }
-//
-//
-// fn process_line(alm: &mut Almanach, line: &str) {
-//     match line {
-//         l if l.is_empty() => {
-//             alm.push_new_dm();
-//         }
-//         l if l.chars().next().unwrap_or_default().is_ascii_digit() => {
-//             let (dest, src, len) = l
-//                 .split_whitespace()
-//                 .map(parse_to_u32)
-//                 .collect_tuple()
-//                 .expect("3 numbers should be present per line!");
-//             for i in 0..len {
-//                 alm.insert_into_dm(src + i, dest + i);
-//             }
-//         }
-//         _ => {}
-//     }
-// }
 
 #[derive(Clone, Debug)]
-struct Chunk<'ch> {
+struct DataChunk<'ch> {
     data: Vec<&'ch str>,
 }
-impl<'ch> Chunk<'ch> {
+impl<'ch> DataChunk<'ch> {
     fn new() -> Self {
-        Chunk { data: Vec::new() }
+        DataChunk { data: Vec::new() }
     }
     fn clear(&mut self) {
         self.data.clear();
@@ -113,7 +30,7 @@ fn parse_to_u32(input: &str) -> u32 {
         .expect("Number should be parsable to u32")
 }
 
-fn chunkify(input: &str) -> Vec<Chunk<'_>> {
+fn chunkify(input: &str) -> Vec<DataChunk<'_>> {
     let lines_len = input.lines().count();
 
     input
@@ -121,7 +38,7 @@ fn chunkify(input: &str) -> Vec<Chunk<'_>> {
         .enumerate()
         .skip(3)
         .fold(
-            (Vec::new(), Chunk::new()),
+            (Vec::new(), DataChunk::new()),
             |(mut coll, mut chunk), (i, line)| {
                 if line.chars().next().unwrap_or_default().is_ascii_digit() {
                     chunk.push(line);
@@ -136,23 +53,33 @@ fn chunkify(input: &str) -> Vec<Chunk<'_>> {
         .0
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let seed_map: Vec<u32> = input
-        .lines()
-        .take(1)
-        .flat_map(|line| {
-            let (_, nums) = line.split_once(": ").expect("Should be splitable by ': '");
-            nums.split_whitespace().map(parse_to_u32)
-        })
-        .collect_vec();
+// TODO: Make multithreaded
+fn proccess_seed(seed: &u32, chunks: &Vec<DataChunk>) -> u32 {
+    let mut num_op = *seed;
+    for chunk in chunks {
+        for &line in chunk.iter() {
+            let (dest, src, len) = line
+                .split_whitespace()
+                .map(parse_to_u32)
+                .collect_tuple()
+                .expect("There should be 3 numbers on a line!");
+            if num_op >= src && num_op < src + len {
+                num_op = num_op - src + dest;
+                break;
+            }
+        }
+    }
+    num_op
+}
 
-    let chunks = chunkify(input);
-
-    let res_map = seed_map
+fn proccess_seeds(seed_map: Vec<u32>, chunks: &Vec<DataChunk>) -> BTreeSet<u32> {
+    seed_map
         .iter()
-        .map(|seed| {
+        .enumerate()
+        .map(|(_i, seed)| {
+            // println!("Processing seed number: {i}");
             let mut num_op = *seed;
-            for chunk in &chunks {
+            for chunk in chunks {
                 for &line in chunk.iter() {
                     let (dest, src, len) = line
                         .split_whitespace()
@@ -167,14 +94,49 @@ pub fn part_one(input: &str) -> Option<u32> {
             }
             num_op
         })
-        .collect::<BTreeSet<_>>();
+        .collect::<BTreeSet<_>>()
+}
+pub fn part_one(input: &str) -> Option<u32> {
+    let seed_map: Vec<u32> = input
+        .lines()
+        .take(1)
+        .flat_map(|line| {
+            let (_, nums) = line.split_once(": ").expect("Should be splitable by ': '");
+            nums.split_whitespace().map(parse_to_u32)
+        })
+        .collect_vec();
+
+    let chunks = chunkify(input);
+
+    let res_map = proccess_seeds(seed_map, &chunks);
 
     res_map.first().copied()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let _ = input;
-    None
+    let seed_map = input
+        .lines()
+        .take(1)
+        .flat_map(|line| {
+            let (_, nums) = line.split_once(": ").expect("Should be splitable by ': '");
+            nums.split_whitespace()
+                .map(parse_to_u32)
+                .tuples::<(_, _)>()
+                .flat_map(|(seed_num, len)| seed_num..seed_num + len)
+        })
+        .collect_vec();
+
+    println!("Collected seed map - LEN: {}", seed_map.len());
+    let chunks = chunkify(input);
+    println!("Chunkified - LEN: {}", chunks.len());
+
+    let res_map = seed_map
+        .par_iter()
+        .map(|seed| proccess_seed(seed, &chunks))
+        .collect::<BTreeSet<_>>();
+    println!("Seeds processed");
+
+    res_map.first().copied()
 }
 
 #[cfg(test)]
@@ -190,6 +152,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(46));
     }
 }
